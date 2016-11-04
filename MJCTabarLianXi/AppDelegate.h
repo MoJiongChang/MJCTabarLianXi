5267f95986707242f07e8b55fe9ea8c96425e844
gdshwhl007 <gdshwhl007@gdshwhl007demac-mini.local>
2016-11-04 11:01:50 +0800

    Initial Commit

---
+++ MJCTabarLianXi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJCTabarLianXi
+//
+//  Created by gdshwhl007 on 16/11/4.
+//  Copyright © 2016年 gdshwhl007. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MJCTabarLianXi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MJCTabarLianXi
+//
+//  Created by gdshwhl007 on 16/11/4.
+//  Copyright © 2016年 gdshwhl007. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
